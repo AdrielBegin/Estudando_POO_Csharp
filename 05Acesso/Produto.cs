127c8d71d9a25820cf8567d188679dc9b83e08b0
Adriel Pereira <adriel.pemi@gmail>
2023-03-23 11:32:44 -0400

    Acesso

---
+++ 05Acesso/Produto.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Produto
+{
+    /*
+         Modificadores de acesso - Visualizar os atributos ou metodos
+         Public = Atributos e Métodos visível para qualquer classe
+         Private = Atributos e Métodos visível apenas na classe onde são criada
+         Protected = Atributos e Métodos visível em classes onde são criados ou herdados
+    */
+    public string nome;
+    private double valor;
+    
+
+}
---
+++ 05Acesso/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main( string[] args)
+    {
+        Produto p = new Produto();        
+    }
+
+}
